dffd391cd31eaccd9284d389336d23dcad8bec39
asdfg5415 <asdfg5415@gmail.com>
2021-04-29 11:44:40 +0900

    [FEAT]: RESPONSE 형식 결정
    ### 응답 개수에 따라서 CommonResult 를 상속
    - SinegleResult
    - MultipleResult
    
    ### ResponseForm 을 정적 메소드로 구현
    
    Resolves: #7

--- src/main/java/com/sns/zuzuclub/controller/post/PostController.java
+++ src/main/java/com/sns/zuzuclub/controller/post/PostController.java
@@ -1,13 +1,14 @@
 package com.sns.zuzuclub.controller.post;
 
 
+import com.sns.zuzuclub.global.response.ResponseForm;
+import com.sns.zuzuclub.global.response.SingleResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class PostController {
 
   @GetMapping("/post")
@@ -16,7 +17,7 @@ public class PostController {
   }
 
   @GetMapping("/var/{some}")
-  public String getVar(@PathVariable String some, @RequestParam String param) {
-    return some + param;
+  public SingleResult<String> getVar(@PathVariable String some, @RequestParam String param) {
+    return ResponseForm.getSingleResult(some+param, "성공띠");
   }
 }
