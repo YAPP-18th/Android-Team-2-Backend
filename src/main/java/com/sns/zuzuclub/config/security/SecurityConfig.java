a1c45c127619a1e45357f071a0622ae31feba49c
asdfg5415 <asdfg5415@gmail.com>
2021-04-30 01:06:20 +0900

    [FIX]: config 패키지 구조 변경
    - 스웨거 패키지 생성
    - security 패키지 통합
    
    Resolves: #4

--- src/main/java/com/sns/zuzuclub/config/security/SecurityConfig.java
+++ src/main/java/com/sns/zuzuclub/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.sns.zuzuclub.security;
+package com.sns.zuzuclub.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- src/main/java/com/sns/zuzuclub/config/swagger/SwaggerConfig.java
+++ src/main/java/com/sns/zuzuclub/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.sns.zuzuclub.config;
+package com.sns.zuzuclub.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
