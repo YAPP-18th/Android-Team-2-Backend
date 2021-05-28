9652438bbb8c727cc68c7b0a64cf71a52e282e42
asdfg5415 <asdfg5415@gmail.com>
2021-05-29 02:13:01 +0900

    [FEAT]: StockService - 관심 종목 추가

--- src/main/java/com/sns/zuzuclub/domain/stock/application/StockService.java
+++ src/main/java/com/sns/zuzuclub/domain/stock/application/StockService.java
@@ -6,7 +6,9 @@ import com.sns.zuzuclub.domain.stock.model.Stock;
 import com.sns.zuzuclub.domain.stock.repository.StockRepository;
 import com.sns.zuzuclub.domain.user.helper.UserHelper;
 import com.sns.zuzuclub.domain.user.model.User;
+import com.sns.zuzuclub.domain.user.model.UserStockScrap;
 import com.sns.zuzuclub.domain.user.repository.UserRepository;
+import com.sns.zuzuclub.domain.user.repository.UserStockScrapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,7 @@ public class StockService {
 
   private final StockRepository stockRepository;
   private final UserRepository userRepository;
+  private final UserStockScrapRepository userStockScrapRepository;
 
   public StockResponseDto getStock(Long userId, Long stockId) {
     User user = UserHelper.findUserById(userRepository, userId);
@@ -26,9 +29,14 @@ public class StockService {
 
   @Transactional
   public void createScrapStock(Long userId, Long stockId) {
+    User user = UserHelper.findUserById(userRepository, userId);
+    Stock stock = StockHelper.findStockById(stockRepository, stockId);
+    UserStockScrap userStockScrap = new UserStockScrap(user, stock);
+    userStockScrapRepository.save(userStockScrap);
   }
 
   @Transactional
   public void deleteScrapStock(Long userId, Long stockId) {
+
   }
 }
