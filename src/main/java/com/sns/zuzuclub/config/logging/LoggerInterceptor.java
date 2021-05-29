93a5219d2e1d1a8b199c1ebc95228a5533b03762
asdfg5415 <asdfg5415@gmail.com>
2021-05-29 19:36:20 +0900

    [FEAT]: 로그 설정 추가

---
+++ src/main/java/com/sns/zuzuclub/config/logging/LoggerInterceptor.java
@@ -0,0 +1,38 @@
+package com.sns.zuzuclub.config.logging;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    logger.info("url : {}", getFullURL(request));
+    return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    logger.info("response status: {}", response.getStatus());
+  }
+
+  private String getFullURL(HttpServletRequest request) {
+    StringBuilder requestURL = new StringBuilder(request.getRequestURL().toString());
+    String queryString = request.getQueryString();
+
+    if (queryString == null) {
+      return requestURL.toString();
+    } else {
+      return requestURL.append('?').append(queryString).toString();
+    }
+  }
+}
+
