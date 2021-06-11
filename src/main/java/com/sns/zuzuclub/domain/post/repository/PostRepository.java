3e7692d01846b5e197f0828c3fe29eb6c022a046
asdfg5415 <asdfg5415@gmail.com>
2021-06-12 04:06:54 +0900

    [FIX]: 피드 - Response에 페이징 처리를 위한 필드 추가(#34)
    resolves: #34

--- src/main/java/com/sns/zuzuclub/domain/post/repository/PostRepository.java
+++ src/main/java/com/sns/zuzuclub/domain/post/repository/PostRepository.java
@@ -4,11 +4,12 @@ import com.sns.zuzuclub.domain.post.model.Post;
 import com.sns.zuzuclub.domain.user.model.User;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-  List<Post> findByCreatedAtAfter(LocalDateTime createdAt, Pageable pageable);
+  Page<Post> findByCreatedAtAfter(LocalDateTime createdAt, Pageable pageable);
   List<Post> findAllByCreatedAtAfter(LocalDateTime createdAt);
-  List<Post> findAllByUserIn(List<User> userList, Pageable pageable);
+  Page<Post> findAllByUserIn(List<User> userList, Pageable pageable);
 }
