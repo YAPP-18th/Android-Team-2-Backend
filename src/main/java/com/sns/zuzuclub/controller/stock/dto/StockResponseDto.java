0311246d2f3e9cb982ccb198774b9226d4e2b9fe
asdfg5415 <asdfg5415@gmail.com>
2021-06-24 12:05:31 +0900

    [FIX]: PostResponseList 정렬방식 수정(#50)
    resolves: #50

--- src/main/java/com/sns/zuzuclub/controller/stock/dto/StockResponseDto.java
+++ src/main/java/com/sns/zuzuclub/controller/stock/dto/StockResponseDto.java
@@ -5,7 +5,9 @@ import com.sns.zuzuclub.controller.post.dto.PostResponseDto;
 import com.sns.zuzuclub.domain.post.model.Post;
 import com.sns.zuzuclub.domain.stock.model.Stock;
 import com.sns.zuzuclub.domain.user.model.User;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -27,7 +29,10 @@ public class StockResponseDto {
     this.todayEmotionValue = stock.getStockEmotionRatio();
     this.isScraped = user.isScrapedStock(stock);
 
-    List<Post> postList = stock.getPostList();
+    List<Post> postList = stock.getPostList()
+                               .stream()
+                               .sorted(Comparator.comparing(Post::getCreatedAt))
+                               .collect(Collectors.toList());
     this.postResponseDtoList = PostResponseDto.ListFrom(postList, user.getId());
   }
 }
