0d0ccaa699a9b8458a847261b2d8b6eea12e4d2e
asdfg5415 <asdfg5415@gmail.com>
2021-06-24 13:15:25 +0900

    [FIX]: 피드 - 글쓰기 RequestDto의 종목 자료형 수정(#52)
    resolves: #52

--- src/main/java/com/sns/zuzuclub/domain/stock/repository/StockRepository.java
+++ src/main/java/com/sns/zuzuclub/domain/stock/repository/StockRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+  List<Stock> findAllByStockNameIn(List<String> stockNameList);
   List<Stock> findAllByStockNameStartsWith(String stockName);
   List<Stock> findAllByTotalCountIsGreaterThan(int totalCount);
 
