95211c6b300c09d32b18224a8e4b99a166e287f9
asdfg5415 <asdfg5415@gmail.com>
2021-07-02 11:13:18 +0900

    [FEAT]: 팔로우 - 팔로우 취소하기(#58)
    resolves: #58

--- src/main/java/com/sns/zuzuclub/global/exception/errorCodeType/UserErrorCodeType.java
+++ src/main/java/com/sns/zuzuclub/global/exception/errorCodeType/UserErrorCodeType.java
@@ -8,7 +8,8 @@ public enum UserErrorCodeType {
   INVALID_USER(431, "유효하지 않은 유저"),
   INVALID_REFRESH_TOKEN(432, "유효하지 않은 재발급 토큰"),
   DUPLICATE_NICKNAME(433, "중복된 닉네임"),
-  EMPTY_NICKNAME(434, "빈 문자열 닉네임");
+  EMPTY_NICKNAME(434, "빈 문자열 닉네임"),
+  INVALID_FOLLOW_INFO(435, "유효하지 않은 팔로우 정보");
 
   private final int errorCode;
   private final String message;
